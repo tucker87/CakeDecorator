4faf01129c5e52cc1a71935bb725a5195ff6886b
tucker87 <jtgeek@gmail.com>
2014-06-23 23:23:13 -0400

    First Run

---
+++ CakeDecorator/CakeDecorator.cs
@@ -0,0 +1,61 @@
+﻿namespace CakeDecorator
+{
+    public class ChocolateCake : ICake
+    {
+        public double GetPrice() { return 1.5; }
+
+        public string Desc { get { return "Chocolate Cake"; } }
+        public string Name { get { return "C"; } }
+
+        public string GetCakeName()
+        {
+            return Name;
+        }
+    }
+
+    public class WhiteCake : ICake
+    {
+        public double GetPrice() { return 1; }
+
+        public string Desc { get { return "White Cake"; } }
+        public string Name { get { return "W"; } }
+    }
+
+    public interface ICake
+    {
+        double GetPrice();
+        string Desc { get; }
+    }
+
+    public abstract class Toppings : ICake
+    {
+        private readonly ICake _cake;
+
+        protected Toppings(ICake cake) { _cake = cake; }
+
+        public virtual double GetPrice() { return _cake.GetPrice(); }
+
+        public virtual string Desc { get { return _cake.Desc; } }
+    }
+
+    public class Sprinkles : Toppings
+    {
+        public Sprinkles(ICake cake) : base(cake) { }
+
+        public override double GetPrice() { return base.GetPrice() + 1; }
+
+        public override string Desc { get { return base.Desc + " with Sprinkles"; } }
+
+        public string GetCakeName()
+        {
+            return Name + "S";
+        }
+    }
+
+    public class Topper : Toppings
+    {
+        public Topper(ICake cake) : base(cake) { }
+
+        public override double GetPrice() { return base.GetPrice() + 2; }
+    }
+}
---
+++ CakeDecorator/CakeDecoratorTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CakeDecorator;
+
+namespace CakeDecorator
+{
+    [TestClass]
+    public class CakeDecoratorTests
+    {
+
+        //Define bases
+        [TestMethod]
+        public void WhiteCakesPriceIsOne()
+        {
+            var whiteCake = new WhiteCake();
+            Assert.AreEqual(1, whiteCake.GetPrice());
+        }
+
+        [TestMethod]
+        public void ChocolateCakesPriceIsOneFifty()
+        {
+            var chocolateCake = new ChocolateCake();
+            Assert.AreEqual(1.5, chocolateCake.GetPrice());
+        }
+        //Define Decorators
+        [TestMethod]
+        public void ChocolateCakeWithSprinklesIsTwoFifty()
+        {
+            var chocolateCake = new ChocolateCake();
+            var chocolateCakeWithSprinkles = new Sprinkles(chocolateCake);
+            Assert.AreEqual(2.5, chocolateCakeWithSprinkles.GetPrice());
+        }
+
+        [TestMethod]
+        public void WhiteCakeWithSprinklesIsTwo()
+        {
+            var whiteCake = new WhiteCake();
+            var whiteCakeWithSprinkles = new Sprinkles(whiteCake);
+            Assert.AreEqual(2, whiteCakeWithSprinkles.GetPrice());
+        }
+
+        [TestMethod]
+        public void WhiteCakeWithTopperIsThree()
+        {
+            var whiteCake = new WhiteCake();
+            var whiteCakeWithTopper = new Topper(whiteCake);
+            Assert.AreEqual(3, whiteCakeWithTopper.GetPrice());
+        }
+
+        //Adding field to test if it acts like methods
+
+        [TestMethod]
+        public void CakesHaveADescription()
+        {
+            var whiteCake = new WhiteCake();
+            Assert.AreEqual("White Cake", whiteCake.Desc);
+        }
+
+        [TestMethod]
+        public void ToppingsAddToTheDescription()
+        {
+            var chocolateCake = new ChocolateCake();
+            var chocolateCakeWithSprinkles = new Sprinkles(chocolateCake);
+            Assert.AreEqual("Chocolate Cake with Sprinkles", chocolateCakeWithSprinkles.Desc);
+        }
+
+        //Adding a field that the Toppings would only reference
+        [TestMethod]
+        public void CakesHaveNames()
+        {
+            var whiteCake = new WhiteCake();
+            var cakeName = whiteCake.Name;
+            Assert.AreEqual("W", cakeName);
+        }
+
+        [TestMethod]
+        public void GetCakeNameReturnsCakeName()
+        {
+            var chocolateCake = new ChocolateCake();
+            var cakeName = chocolateCake.GetCakeName();
+            Assert.AreEqual("C", cakeName);
+        }
+
+        [TestMethod]
+        public void GetCakeNameWorksWithToppings()
+        {
+            var whiteCake = new WhiteCake();
+            var whiteCakeWithSprinkles = new Sprinkles(whiteCake);
+            var cakeName = whiteCakeWithSprinkles.GetCakeName();
+            Assert.AreEqual("WS", cakeName);
+        }
+    }
+}
